a56bc294bff1a1f3156cd30cbaded78170a8581b
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-06-14 10:43:03 +0530

    last commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="root">
+      <h1>Welcome to react</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
   )
 }
 
